4111dd57cb15c3442358c8aea1f483528d7cd771
dhheeraj <dhheeraj@gmail.com>
2019-08-30 12:50:42 +0530

    added production files

---
+++ build/precache-manifest.e5ec6fe19e9f368bdde52c5589270878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013ca9855e26fd2f2130e6e37088104f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc7f2c13e227fdded209",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8c3e02e8c5abc3029558",
+    "url": "/static/js/2.00643331.chunk.js"
+  },
+  {
+    "revision": "fc7f2c13e227fdded209",
+    "url": "/static/js/main.d9ba80c2.chunk.js"
+  },
+  {
+    "revision": "c97b24c36df0c39221fd",
+    "url": "/static/js/runtime~main.8e12ad88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
